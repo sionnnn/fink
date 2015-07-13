6a3c14d9b12a1dd5d157607527216aab1f061149
sion evans <sionevans@gmail.com>
2015-07-13 16:35:05 +0100

    rebuild

--- public/assets/js/webworkers/worker.js
+++ public/assets/js/webworkers/worker.js
@@ -1,7 +1,10 @@
+'use strict';
+
 function echo(str) {
-    console.log('web worker .ts ' + str);
+	console.log('web worker test .ts ' + str);
 }
+
 self.onmessage = function (e) {
-    var x = e.data.value;
-    echo(x);
+	var x = e.data.value;
+	echo(x);
 };
